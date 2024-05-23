e3b0a5596051f1af89fd0745e37d473822b9e257
Antoine Mouly <amouly@k0r3p4.42mulhouse.fr>
2024-05-23 17:21:40 +0200

    variance

--- Data_Scientist/correlation.py
+++ Data_Scientist/correlation.py
@@ -5,7 +5,7 @@ from scipy.stats import pointbiserialr
 
 data = pd.read_csv('Train_knight.csv')
 
-data['knight'] = data['knight'].map({'Jedi': 0, 'Sith': 1})
+data['knight'] = data['knight'].map({'Jedi': 1, 'Sith': 0})
 correlations = data.corr(method='spearman')
 knight_correlations = correlations['knight']
 absolute_correlations = knight_correlations.abs()
@@ -20,3 +20,11 @@ for col in data.columns[:]:
 
 sorted_correl = pd.Series({k: v for k, v in sorted(correl.items(), key=lambda item: item[1], reverse=True)})
 sorted_correl
+
+#heatmap
+plt.figure(figsize=(10, 10))
+plt.matshow(data.corr(method='spearman'), fignum=1, cmap='Reds_r')
+plt.xticks(range(data.shape[1]), data.columns, fontsize=14, rotation=90)
+plt.yticks(range(data.shape[1]), data.columns, fontsize=14)
+plt.colorbar()
+plt.show()
